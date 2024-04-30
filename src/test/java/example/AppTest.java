59266d7ee769ad9c506c313d31c9681725a30fae
hishamkhattab1 <hisham.s.1410@gmail.com>
2024-04-30 23:02:41 +0300

    commit

--- src/test/java/example/AppTest.java
+++ src/test/java/example/AppTest.java
@@ -17,7 +17,7 @@ public class AppTest
        
         System.out.println("Testin");
 
-        URL hubUrl = new URL("http://localhost:4444/wd/hub");
+        URL hubUrl = new URL("http://192.168.100.44:4444/wd/hub");
         
         ChromeOptions options = new ChromeOptions();
         
