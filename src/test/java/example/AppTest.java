e75b656010114fbc86fe7a8c0bea863c2efa2f36
hishamkhattab1 <hisham.s.1410@gmail.com>
2024-04-30 19:36:44 +0300

    first commit

---
+++ src/test/java/example/AppTest.java
@@ -0,0 +1,15 @@
+package example;
+
+import org.testng.annotations.Test;
+
+
+
+public class AppTest 
+{
+    @Test
+    public void Testing()
+    {
+        System.out.println("Testing");
+    }
+}
+   
