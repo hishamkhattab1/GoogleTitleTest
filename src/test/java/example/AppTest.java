f94723f7357702116b971d88e5cebab1fc0b50cf
hishamkhattab1 <hisham.s.1410@gmail.com>
2024-04-30 20:02:34 +0300

    first commit

--- src/test/java/example/AppTest.java
+++ src/test/java/example/AppTest.java
@@ -1,15 +1,46 @@
 package example;
 
-import org.testng.annotations.Test;
+import java.net.MalformedURLException;
 
+import org.testng.annotations.Test;
+import java.net.URL;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 
 public class AppTest 
 {
     @Test
-    public void Testing()
-    {
-        System.out.println("Testing");
+    public void Testing() throws MalformedURLException{
+       
+        System.out.println("Testin");
+
+        URL hubUrl = new URL("http://192.168.9.128:4444/wd/hub");
+        
+        ChromeOptions options = new ChromeOptions();
+        
+        options.addArguments("--headless"); 
+
+        WebDriver driver = new ChromeDriver(options);
+        
+        driver = new RemoteWebDriver(hubUrl, options);
+        
+        driver.get("http://www.google.com");
+
+        String actualTitle = driver.getTitle();
+
+        System.out.println(actualTitle);
+
+        String expectedTitle = "Google";
+
+        if (actualTitle.equals(expectedTitle)) {
+            System.out.println("Test Passed! Title is: " + actualTitle);
+        } else {
+            System.out.println("Test Failed. Expected title: " + expectedTitle + ", Actual title: " + actualTitle);
+        }
+        driver.quit();
     }
 }
    
