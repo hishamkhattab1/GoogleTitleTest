eb0523ef460e5fefd696a1a44864baf385e78a2e
hishamkhattab1 <hisham.s.1410@gmail.com>
2024-05-06 18:37:00 +0300

    commit

--- StartEnvironment.sh
+++ StartEnvironment.sh
@@ -5,7 +5,9 @@ SERVER="root@192.168.100.44" # replace with your username and hostname
 FILE_PATH="~/docker-compose.yaml" # replace with your file path
 
 # Connect to the server and perform operations
-ssh -tt $SERVER << EOF
-    scp $SERVER:$FILE_PATH ./docker-compose.yaml
-    docker compose -f docker-compose.yaml up -d
-EOF
+#ssh -tt $SERVER << EOF
+#    scp $SERVER:$FILE_PATH ./docker-compose.yaml
+#    docker compose -f docker-compose.yaml up -d
+#EOF
+scp $SERVER:$FILE_PATH ./docker-compose.yaml
+ssh -tt $SERVER 'docker compose -f docker-compose.yaml up -d
