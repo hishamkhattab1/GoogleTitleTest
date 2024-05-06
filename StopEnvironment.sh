bcfbfc21af303fb910ee234b7971b6f055b55273
hishamkhattab1 <hisham.s.1410@gmail.com>
2024-05-06 12:51:08 +0300

    commit

--- StopEnvironment.sh
+++ StopEnvironment.sh
@@ -4,7 +4,7 @@
 SERVER="root@192.168.100.44" # replace with your username and hostname
 
 # Connect to the server and perform operations
-ssh -tt $SERVER
-docker compose down
-rm ./docker-compose.yaml
-exit
+ssh -tt $SERVER << EOF
+    docker compose down
+    rm ./docker-compose.yaml
+EOF
